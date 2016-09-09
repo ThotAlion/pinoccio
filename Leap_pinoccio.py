0305fb3c3ac38a1500d1a78e250fd883329c5ccc
ThotAlion <thomas.peyruse@gmail.com>
2016-09-09 15:39:19 +0200

    Leap test tuned

--- Leap_pinoccio.py
+++ Leap_pinoccio.py
@@ -5,22 +5,29 @@ import serial
 ser = serial.Serial(
     port='COM9',
     baudrate=115200,
+    write_timeout=0,
     timeout=0)
 
-motor = 3
+Hp=0
 l = Leap.Controller()
-
 while True:
     #print "debut frame"
     f = l.frame()
     #print "fin frame"
     for h in f.hands:
-        if h.is_left:
-            H = int(h.palm_position.y)
-            line = "M"+str(motor)+"P"+str(H)+"\n"
-            #print "debut write"
+        if h.is_right:
+            H = h.palm_position.y*5
+            ser.reset_output_buffer()
+            ser.reset_input_buffer()
+            # filtrage
+            if Hp > 0:
+                Hc=int(round(0.01*H+0.99*Hp))
+                print Hc
+            else:
+                Hc=H
+            Hp=Hc
+            motor = 3
+            line = "M"+str(motor)+"P"+str(Hc)+"\n"
             ser.write(line)
-            #print "fin write"
-            #print line
             
-    time.sleep(0.02)
+    time.sleep(0.01)
